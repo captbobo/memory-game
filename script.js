43cc539fc9eb621b57b229b9a03a2fc66c782e11
Can Sürmeli <cansurmeli@gmail.com>
2018-06-09 02:13:34 +0300

    refactor: Create card divs with JS

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-window.onload = function() {
 
+window.onload = function() {
+  let lvl = 16;
+  for(let i = 1; i <= lvl; i++){
+    let div = document.createElement("div");
+    div.setAttribute("class", "card");
+    div.innerHTML = i;
+    document.querySelector(".container").appendChild(div);
+  }
+
+// click animation
   let cards = document.getElementsByClassName("card");
-  [].forEach.call(cards,function(element, index){
-    cards[index].addEventListener("click", function() {
-      this.style.background = "#ccc";
+  [].forEach.call(cards,function(element){
+    element.addEventListener("click", function() {
+      //    to do after click
     });
   });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 
 }
