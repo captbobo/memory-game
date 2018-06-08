a26d49e5e7ef14e62a920bfbf35e8e476b27b2ab
Can Sürmeli <cansurmeli@gmail.com>
2018-06-08 16:01:02 +0300

    feat: add onclick functionality

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.onload = function() {
 
+  let cards = document.getElementsByClassName("card");
+  [].forEach.call(cards,function(element, index){
+    cards[index].addEventListener("click", function() {
+      this.style.background = "#ccc";
+    });
+  });
+
+
+}
