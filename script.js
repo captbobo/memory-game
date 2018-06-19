d9e7a6811b9988dafa21a5ba8ba58251bb929c92
Can Sürmeli <cansurmeli@gmail.com>
2018-06-19 16:45:41 +0300

    style: add comments and correct indentation

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ window.onload = function() {
 
   scene.setAttribute("class", "scene");
   // returns a shuffled array of values
-  // the length and hence the 'level' of the game depends on this value
-  // the value should only be n-squared 
   valuesArray = valueArray(16);
 
-  //
   // create them flippin' cards
   for(let i = 1 ; i <= valuesArray.length ; i++){
     let cardFaceFront = document.createElement("div"),
@@ -28,6 +25,7 @@ window.onload = function() {
 
   document.body.appendChild(scene);
 
+  // addEventListener "click" for all cards
   cards = document.getElementsByClassName("card");
   cardsArray = Array.from(cards);
   cardsArray.forEach(function(elm){
@@ -36,36 +34,37 @@ window.onload = function() {
         });
   })
 
-function valueArray(lvl) {
-  let valueArray = [],
-      finalArray = [];
+  // the length of the array and hence the 'lvl' of the game
+  // depends on this value, it should only be equal to n-squared
+  function valueArray(lvl) {
+    let valueArray = [],
+        finalArray = [];
 
-  for (let i = 1 ; i <= lvl/2 ; i++){
-    valueArray.push(i);
-  }
-  for (let i = 0; i < 2 ; i++){
-    for (let j of valueArray){
-      finalArray.push(valueArray[j-1]);
+    for (let i = 1 ; i <= lvl/2 ; i++){
+      valueArray.push(i);
     }
-  shuffleArray(finalArray);
-  }
-  return finalArray;
-}
-
-
-// random integer generator with a max range
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-// Durstenfeld or Knuth's (or Fisher-Yates) shuffling algorithm
-// From Laurens Holst's answer:
-// https://stackoverflow.com/a/12646864/9144800
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+    for (let i = 0; i < 2 ; i++){
+      for (let j of valueArray){
+        finalArray.push(valueArray[j-1]);
+      }
+    shuffleArray(finalArray);
     }
-}
+    return finalArray;
+  }
+
+  // random integer generator with a max range
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  // Durstenfeld or Knuth's (or Fisher-Yates) shuffling algorithm
+  // From Laurens Holst's answer:
+  // https://stackoverflow.com/a/12646864/9144800
+  function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+      }
+  }
 
 }
